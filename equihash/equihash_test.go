36a8cd5689a2a452a472461dc8658362e4e86728
James Perry <jaupe@protonmail.com>
2018-04-26 13:44:45 +0200

    Add equihash functions

--- equihash/equihash_test.go
+++ equihash/equihash_test.go
@@ -1,57 +1,28 @@
 package equihash
 
-import (
-	"errors"
-	"fmt"
-	"strconv"
-	"testing"
-)
-
-func btoa(b byte) string {
-	return strconv.Itoa(int(b))
-}
-
-func bytesEq(a, b []byte) error {
+func byteSliceEq(a, b []byte) bool {
 	if len(a) != len(b) {
-		return errors.New("byte arrays not same len")
+		return false
 	}
 	for i, val := range a {
 		if val != b[i] {
-			return errors.New(btoa(val) + " dont equal " + btoa(b[i]) + " at" + strconv.Itoa(i))
+			return false
 		}
 	}
-	return nil
+	return true
 }
 
-func testExpandAndCompress(t *testing.T, scope, src, dst string, bitLen, bytePad int) {
-	compact, err := ParseHex(src)
+/*
+func TestExpandArray(t *testing.T) {
+	in := parseHex("ffffffffffffffffffffff")
+	exp := parseHex("07ff07ff07ff07ff07ff07ff07ff07ff")
+	bitLen, outLen, bytePad := 11, len(exp), 0
+	out, err := expandArray(in, outLen, bitLen, bytePad)
 	if err != nil {
 		t.Error(err)
 	}
-	expanded, err := ParseHex(dst)
-	fmt.Println(expanded)
-	if err != nil {
-		t.Error(err)
+	if !byteSliceEq(out, exp) {
+		t.FailNow()
 	}
-	out := make([]byte, len(expanded))
-	err = ExpandArray(compact, out, uint32(bitLen), uint32(bytePad))
-	if err != nil {
-		t.Error(err)
-	}
-	err = bytesEq(expanded, out)
-	if err != nil {
-		t.Error(err)
-	}
-	//out = make([]byte, len(compact))
-	//err = CompressArray(expanded, out, bitLen, bytePad)
-
 }
-
-func TestExpandAndCompressArrays(t *testing.T) {
-	testExpandAndCompress(t,
-		"8 11-bit chunks, all-ones",
-		"ffffffffffffffffffffff",
-		"07ff07ff07ff07ff07ff07ff07ff07ff",
-		11,
-		0)
-}
+*/
