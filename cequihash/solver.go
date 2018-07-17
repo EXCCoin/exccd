8b65b4759f91d3124a0c91e57e407d0bb1a82de6
Sergiy Yevtushenko <Sergiy.Yevtushenko@pragmaticcoders.com>
2018-07-17 15:46:50 +0200

    Try another flags for C++

--- cequihash/solver.go
+++ cequihash/solver.go
@@ -5,7 +5,7 @@
 package cequihash
 
 /*
-#cgo CXXFLAGS: -O3 -march=native -std=c++11 -Wall -Wno-shift-count-overflow -Werror
+#cgo CXXFLAGS: -O3 -march=native -std=c++17 -Wall -Wno-strict-aliasing -Wno-shift-count-overflow -Werror
 #include "cequihash.h"
 */
 import "C"
